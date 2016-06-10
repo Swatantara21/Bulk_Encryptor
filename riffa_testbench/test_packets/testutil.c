6088ece5dd46bbb042ae3a170845e5ec98655d3a
root <root@emsys8.ee.iitb.ac.in>
2016-06-11 00:31:36 +0530

    modified test_packets

--- riffa_testbench/test_packets/testutil.c
+++ riffa_testbench/test_packets/testutil.c
@@ -7,9 +7,13 @@
 //#ifndef SW
 //#include "vhdlCStubs.h"
 //#endif
+#define DATA_CH_SIZE 258 
+//2064 bytes = 129 blocks = 258 words (64 bit) = 516 (32 bit) words
 
-uint64_t result[8];
-uint64_t sent_values[8];
+uint64_t result[DATA_CH_SIZE];
+uint64_t sent_values[DATA_CH_SIZE];
+uint64_t cmd_values[8];
+uint64_t cmd_values2[8];
 //uint32_t numWords;
 //fpga_t * fpga;
 //uint32_t sent, recvd, chnl, id;
@@ -55,6 +59,7 @@ int main(int argc, char** argv) {
 			printf("%d: name:%s\n", i, info.name[i]);
 			printf("%d: vendor id:%04X\n", i, info.vendor_id[i]);
 			printf("%d: device id:%04X\n", i, info.device_id[i]);
+			printf("options: \n1: fpga reset\n2: KAT 3: throughput\n");
 		}
 	}
 	else if (option == 1) { // Reset FPGA
@@ -79,10 +84,9 @@ int main(int argc, char** argv) {
 		fpga_close(fpga);
 	}
 	else if (option == 2) { // Send data, receive data
-		printf("OPTION 2 is NOT WORKING when recieving 8 words at a time");
+		printf("OPTION 2");
 		if (argc < 4) {
-			printf("This option is used to get latency (numWords = 1)\n", argv[0], option);
-			printf("Usage: %s %d <fpga id> <chnl>\n", argv[0], option);
+			printf("KAT(Known Answer Test) out chnl = 129 blocks\nUsage: %s %d <fpga id> <chnl> \n", argv[0], option);
 			return -1;
 		}
 
@@ -99,19 +103,45 @@ int main(int argc, char** argv) {
 		}
 
 		k=0;
+		cmd_values[1] = 0x8800000000000080;
+		cmd_values[0] = 0x0000000000000000;
+		cmd_values[2] = 0x0000000000000000;
+		cmd_values[3] = 0x0000000000000000;
+		
 		sent_values[0] = 0x8000000000000000;
 		sent_values[1] = 0x0000000000000000;
-		
+		sent_values[0] = 0x8000000000000000;
+		sent_values[1] = 0x0000000000000000;
+		for(idx = 1; idx < 128; idx++)
+		{	
+			sent_values[2*idx]= sent_values[2*idx - 2];
+			sent_values[2*idx + 1]= sent_values[2*idx - 1];
+			
+			if(sent_values[2*idx + k]==0xffffffffffffffff){
+				k++;
+				sent_values[2*idx + k]= 0x8000000000000000;
+			}
+			else
+				sent_values[2*idx + k] = sent_values[2*idx + k]|(sent_values[2*idx + k]/2);
+				//printf("\nPT: %d: %16lx%16lx",idx,sent_values[2*idx],sent_values[2*idx +1]);
+		}
 		GET_TIME_VAL(0);
 		
-		sent = fpga_send(fpga, chnl, sent_values, 4, 0, 1, 25000);
+		sent = fpga_send(fpga, chnl, cmd_values, 8, 0, 1, 25000);
+		fprintf(stdout,"\ncmd_send_status = %d -- %d ",sent, 1);
+		sent = fpga_send(fpga, chnl, sent_values, 512, 0, 1, 25000);
+		fprintf(stdout,"\ndata_send_status = %d -- %d ",sent, 2);
+		
 		//fprintf(stdout,"\nsend_status = %d -- %d ,    input =%16llx %16llx",sent, idx,sent_values[0],sent_values[1]);
 
-		recvd = fpga_recv(fpga, chnl, result, 4, 25000);
+		recvd = fpga_recv(fpga, chnl, result, 516, 25000);
 		GET_TIME_VAL(1);
-		if (recvd < 4) fprintf(stderr,"\nERROR:-------------------------");
+		if (recvd < 516) fprintf(stderr,"\nERROR:-------------------------");
+		fprintf(stdout,"\nrecv_status = %d -- %d",recvd,3);
+		
 		//fprintf(stdout,"\nrecv_status = %d -- %d ,    output = %16llx %16llx",recvd,idx, result[0],result[1]);
-		//fprintf(stdout,"\n%16llx%16llx",result[0],result[1]);
+		for(idx = 0; idx < 129; idx++)
+			fprintf(stdout,"\n%16llx%16llx",result[2*idx],result[2*idx + 1]);
 
 		printf("\ntime taken (latency): %f ms\n",(TIME_VAL_TO_MS(1) - TIME_VAL_TO_MS(0)));
 				
@@ -120,7 +150,7 @@ int main(int argc, char** argv) {
 	}
 	else if (option == 3) { // Send data, receive data
 		if (argc < 6) {
-			printf("Usage: %s %d <fpga id> <chnl> <channel size in channel tester in bytes> <num words to transfer>\n", argv[0], option);
+			printf("Usage NOT WORKING : %s %d <fpga id> <chnl> <channel size in channel tester in bytes> <num blocks to transfer>\n", argv[0], option);
 			return -1;
 		}
 
@@ -128,7 +158,8 @@ int main(int argc, char** argv) {
 		id = atoi(argv[2]);
 		chnl = atoi(argv[3]);
 		numWords = atoi(argv[5]);
-		ch_size = atoi(argv[4])/4;
+		//ch_size = atoi(argv[4])/4;
+		ch_size = 128;
 		numLoops = numWords/ch_size;
 		remWords = numWords%ch_size;
 		// Get the device with id
@@ -141,6 +172,15 @@ int main(int argc, char** argv) {
 		
 		
 		k=0;
+		
+		cmd_values[1] = 0x8800000000000080;
+		cmd_values[0] = 0x0000000000000000;
+		cmd_values[2] = 0x0000000000000000;
+		cmd_values[3] = 0x0000000000000000;
+		
+		cmd_values2[1] = 0x8800000000000080;
+		cmd_values2[0] = 0x0000000000000000;
+		
 		sent_values[0] = 0x8000000000000000;
 		sent_values[1] = 0x0000000000000000;;
 		for (idx =0; idx<ch_size; idx++){
@@ -150,12 +190,19 @@ int main(int argc, char** argv) {
 		
 		
 		GET_TIME_VAL(0);
-		for(idx = 0; idx < numLoops; idx++)
+		sent = fpga_send(fpga, chnl, cmd_values, 8, 0, 1, 25000);
+		sent = fpga_send(fpga, chnl, sent_values, 4*ch_size, 0, 1, 25000);
+		
+		recvd = fpga_recv(fpga, chnl, result, 4*(ch_size+1), 25000);
+				
+		for(idx = 1; idx < numLoops; idx++)
 		{	
+			sent = fpga_send(fpga, chnl, cmd_values2, 4, 0, 1, 25000);
+		
 			sent = fpga_send(fpga, chnl, sent_values, 4*ch_size, 0, 1, 25000);
 			//for (idx =0; idx<2; idx++)
 			//	fprintf(stdout,"\n%16llx%16llx",sent_values[idx*2],sent_values[idx*2 +1]);
-			recvd = fpga_recv(fpga, chnl, result, 4*ch_size, 25000);
+			recvd = fpga_recv(fpga, chnl, result, 4*(ch_size+1), 25000);
 			//for (idx =0; idx<2; idx++)
 			//	fprintf(stdout,"\n%16llx%16llx",result[idx*2],result[idx*2 +1]);
 			
